e1c189284825fd57befb38177c9aa7727bdd8f1a
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-02-14 16:07:50 +0200

    prettier

--- lib/uws-tracker.ts
+++ lib/uws-tracker.ts
@@ -15,17 +15,28 @@
  */
 
 import { StringDecoder } from "string_decoder";
-import { App, SSLApp, WebSocket, HttpRequest, TemplatedApp, us_socket_context_t, HttpResponse } from "uWebSockets.js";
+import {
+  App,
+  SSLApp,
+  WebSocket,
+  HttpRequest,
+  TemplatedApp,
+  us_socket_context_t,
+  HttpResponse,
+} from "uWebSockets.js";
 import Debug from "debug";
 import { Tracker, TrackerError, PeerContext } from "./tracker.js";
-import { ServerSettings, WebSocketsSettings, WebSocketsAccessSettings } from "./run-uws-tracker.js";
+import {
+  ServerSettings,
+  WebSocketsSettings,
+  WebSocketsAccessSettings,
+} from "./run-uws-tracker.js";
 
 declare module "./tracker.js" {
-    interface PeerContext {
-        ws: WebSocket<PeerContext>;
-    }
+  interface PeerContext {
+    ws: WebSocket<PeerContext>;
+  }
 }
-
 
 const debugWebSockets = Debug("wt-tracker:uws-tracker");
 const debugWebSocketsEnabled = debugWebSockets.enabled;
@@ -39,279 +50,308 @@ const debugRequestsEnabled = debugRequests.enabled;
 const decoder = new StringDecoder();
 
 export interface UwsTrackerSettings {
-    server: ServerSettings;
-    websockets: WebSocketsSettings;
-    access: WebSocketsAccessSettings;
+  server: ServerSettings;
+  websockets: WebSocketsSettings;
+  access: WebSocketsAccessSettings;
 }
 
 export interface PartialUwsTrackerSettings {
-    server?: Partial<ServerSettings>;
-    websockets?: Partial<WebSocketsSettings>;
-    access?: Partial<WebSocketsAccessSettings>;
+  server?: Partial<ServerSettings>;
+  websockets?: Partial<WebSocketsSettings>;
+  access?: Partial<WebSocketsAccessSettings>;
 }
 
 export class UWebSocketsTracker {
-    public readonly settings: UwsTrackerSettings;
-    public readonly tracker: Readonly<Tracker>;
+  public readonly settings: UwsTrackerSettings;
+  public readonly tracker: Readonly<Tracker>;
 
-    private webSocketsCount = 0;
-    private validateOrigin = false;
-    private readonly maxConnections: number;
+  private webSocketsCount = 0;
+  private validateOrigin = false;
+  private readonly maxConnections: number;
 
-    readonly #app: TemplatedApp;
+  readonly #app: TemplatedApp;
 
-    public constructor(tracker: Readonly<Tracker>, settings: PartialUwsTrackerSettings) {
-        this.tracker = tracker;
-        this.settings = {
-            server: {
-                port: 8000,
-                host: "0.0.0.0",
-                ...settings.server,
-            },
-            websockets: {
-                path: "/*",
-                maxPayloadLength: 64 * 1024,
-                idleTimeout: 240,
-                compression: 1,
-                maxConnections: 0,
-                ...settings.websockets,
-            },
-            access: {
-                allowOrigins: undefined,
-                denyOrigins: undefined,
-                denyEmptyOrigin: false,
-                ...settings.access,
-            },
-        };
-
-        this.maxConnections = this.settings.websockets.maxConnections;
-
-        this.validateAccess();
-
-        this.#app = (this.settings.server.key_file_name === undefined)
-            ? App(this.settings.server)
-            : SSLApp(this.settings.server);
-
-        this.buildApplication();
-    }
-
-    public get app(): TemplatedApp {
-        return this.#app;
-    }
-
-    public get stats(): { webSocketsCount: number } {
-        return {
-            webSocketsCount: this.webSocketsCount,
-        };
-    }
-
-    public async run(): Promise<void> {
-        await new Promise<void>(
-            (resolve, reject) => { 
-                this.#app.listen(
-                    this.settings.server.host,
-                    this.settings.server.port,
-                    (token: false | object) => {
-                        if (token === false) {
-                            reject(new Error(
-                                `failed to listen to ${this.settings.server.host}:${this.settings.server.port}`,
-                            ));
-                        } else {
-                            resolve();
-                        }
-                    },
-                );
-            },
-        );
-    }
-
-    private validateAccess(): void {
-        if (this.settings.access.allowOrigins !== undefined) {
-            if (this.settings.access.denyOrigins !== undefined) {
-                throw new Error("allowOrigins and denyOrigins can't be set simultaneously");
-            } else if (!(this.settings.access.allowOrigins instanceof Array)) {
-                throw new Error("allowOrigins configuration paramenters should be an array of strings");
-            }
-        } else if ((this.settings.access.denyOrigins !== undefined) && !(this.settings.access.denyOrigins instanceof Array)) {
-            throw new Error("denyOrigins configuration paramenters should be an array of strings");
-        }
-
-        const origins: readonly string[] | undefined = this.settings.access.allowOrigins ?? this.settings.access.denyOrigins;
-
-
-        if (origins !== undefined) {
-            for (const origin of origins) {
-                if (typeof origin !== "string") {
-                    throw new Error("allowOrigins and denyOrigins configuration paramenters should be arrays of strings");
-                }
-            }
-        }
-
-        this.validateOrigin = (
-            this.settings.access.denyEmptyOrigin
-            || (this.settings.access.allowOrigins !== undefined)
-            || (this.settings.access.denyOrigins !== undefined)
-        );
-    }
-
-    private buildApplication(): void {
-        this.#app.ws(
-            this.settings.websockets.path,
-            {
-                compression: this.settings.websockets.compression,
-                maxPayloadLength: this.settings.websockets.maxPayloadLength,
-                idleTimeout: this.settings.websockets.idleTimeout,
-                open: this.onOpen,
-                upgrade: this.onUpgrade,
-                drain: (ws: WebSocket<PeerContext>) => {
-                    if (debugWebSocketsEnabled) {
-                        debugWebSockets("drain", ws.getBufferedAmount());
-                    }
-                },
-                message: this.onMessage,
-                close: this.onClose,
-            },
-        );
-    }
-
-    private readonly onOpen = (): void => {
-        this.webSocketsCount++;
+  public constructor(
+    tracker: Readonly<Tracker>,
+    settings: PartialUwsTrackerSettings,
+  ) {
+    this.tracker = tracker;
+    this.settings = {
+      server: {
+        port: 8000,
+        host: "0.0.0.0",
+        ...settings.server,
+      },
+      websockets: {
+        path: "/*",
+        maxPayloadLength: 64 * 1024,
+        idleTimeout: 240,
+        compression: 1,
+        maxConnections: 0,
+        ...settings.websockets,
+      },
+      access: {
+        allowOrigins: undefined,
+        denyOrigins: undefined,
+        denyEmptyOrigin: false,
+        ...settings.access,
+      },
     };
 
-    private readonly onUpgrade = (response: HttpResponse, request: HttpRequest, context: us_socket_context_t): void => {
-        if ((this.maxConnections !== 0) && (this.webSocketsCount > this.maxConnections)) {
-            if (debugRequestsEnabled) {
-                debugRequests(
-                    this.settings.server.host,
-                    this.settings.server.port,
-                    "ws-denied-max-connections url:",
-                    request.getUrl(),
-                    "query:",
-                    request.getQuery(),
-                    "origin:",
-                    request.getHeader("origin"),
-                    "total:",
-                    this.webSocketsCount,
-                );
-            }
-            
-            response.close();
-            return;
-        }
+    this.maxConnections = this.settings.websockets.maxConnections;
 
-        if (debugWebSocketsEnabled) {
-            debugWebSockets("connected via URL", request.getUrl());
-        }
+    this.validateAccess();
 
-        if (this.validateOrigin) {
-            const origin = request.getHeader("origin");
+    this.#app =
+      this.settings.server.key_file_name === undefined
+        ? App(this.settings.server)
+        : SSLApp(this.settings.server);
 
-            const shoulDeny = (
-                (this.settings.access.denyEmptyOrigin && (origin.length === 0))
-                || (this.settings.access.denyOrigins?.includes(origin) === true)
-                || (this.settings.access.allowOrigins?.includes(origin) === false)
+    this.buildApplication();
+  }
+
+  public get app(): TemplatedApp {
+    return this.#app;
+  }
+
+  public get stats(): { webSocketsCount: number } {
+    return {
+      webSocketsCount: this.webSocketsCount,
+    };
+  }
+
+  public async run(): Promise<void> {
+    await new Promise<void>((resolve, reject) => {
+      this.#app.listen(
+        this.settings.server.host,
+        this.settings.server.port,
+        (token: false | object) => {
+          if (token === false) {
+            reject(
+              new Error(
+                `failed to listen to ${this.settings.server.host}:${this.settings.server.port}`,
+              ),
             );
+          } else {
+            resolve();
+          }
+        },
+      );
+    });
+  }
 
-            if (shoulDeny) {
-                if (debugRequestsEnabled) {
-                    debugRequests(
-                        this.settings.server.host,
-                        this.settings.server.port,
-                        "ws-denied url:",
-                        request.getUrl(),
-                        "query:",
-                        request.getQuery(),
-                        "origin:",
-                        origin,
-                        "total:",
-                        this.webSocketsCount,
-                    );
-                }
+  private validateAccess(): void {
+    if (this.settings.access.allowOrigins !== undefined) {
+      if (this.settings.access.denyOrigins !== undefined) {
+        throw new Error(
+          "allowOrigins and denyOrigins can't be set simultaneously",
+        );
+      } else if (!(this.settings.access.allowOrigins instanceof Array)) {
+        throw new Error(
+          "allowOrigins configuration paramenters should be an array of strings",
+        );
+      }
+    } else if (
+      this.settings.access.denyOrigins !== undefined &&
+      !(this.settings.access.denyOrigins instanceof Array)
+    ) {
+      throw new Error(
+        "denyOrigins configuration paramenters should be an array of strings",
+      );
+    }
 
-                response.close()
-                return;
-            }
-        }
+    const origins: readonly string[] | undefined =
+      this.settings.access.allowOrigins ?? this.settings.access.denyOrigins;
 
-        if (debugRequestsEnabled) {
-            debugRequests(
-                this.settings.server.host,
-                this.settings.server.port,
-                "ws-open url:",
-                request.getUrl(),
-                "query:",
-                request.getQuery(),
-                "origin:",
-                request.getHeader("origin"),
-                "total:",
-                this.webSocketsCount,
-            );
-        }
-
-        response.upgrade<Pick<PeerContext, 'sendMessage'>>(
-          {
-                sendMessage
-          },
-          request.getHeader('sec-websocket-key'),
-          request.getHeader('sec-websocket-protocol'),
-          request.getHeader('sec-websocket-extensions'),
-          context,
+    if (origins !== undefined) {
+      for (const origin of origins) {
+        if (typeof origin !== "string") {
+          throw new Error(
+            "allowOrigins and denyOrigins configuration paramenters should be arrays of strings",
           );
+        }
+      }
     }
 
-    private readonly onMessage = (ws: WebSocket<PeerContext>, message: ArrayBuffer): void => {
-        debugWebSockets("message of size", message.byteLength);
+    this.validateOrigin =
+      this.settings.access.denyEmptyOrigin ||
+      this.settings.access.allowOrigins !== undefined ||
+      this.settings.access.denyOrigins !== undefined;
+  }
 
-        const userData = ws.getUserData();
-        userData.ws = ws;
+  private buildApplication(): void {
+    this.#app.ws(this.settings.websockets.path, {
+      compression: this.settings.websockets.compression,
+      maxPayloadLength: this.settings.websockets.maxPayloadLength,
+      idleTimeout: this.settings.websockets.idleTimeout,
+      open: this.onOpen,
+      upgrade: this.onUpgrade,
+      drain: (ws: WebSocket<PeerContext>) => {
+        if (debugWebSocketsEnabled) {
+          debugWebSockets("drain", ws.getBufferedAmount());
+        }
+      },
+      message: this.onMessage,
+      close: this.onClose,
+    });
+  }
 
-        let json: object | undefined = undefined;
-        try {
-            json = JSON.parse(decoder.end(new Uint8Array(message) as Buffer)) as object;
-        } catch (e) {
-            debugWebSockets("failed to parse JSON message", e);
-            ws.close();
-            return;
+  private readonly onOpen = (): void => {
+    this.webSocketsCount++;
+  };
+
+  private readonly onUpgrade = (
+    response: HttpResponse,
+    request: HttpRequest,
+    context: us_socket_context_t,
+  ): void => {
+    if (
+      this.maxConnections !== 0 &&
+      this.webSocketsCount > this.maxConnections
+    ) {
+      if (debugRequestsEnabled) {
+        debugRequests(
+          this.settings.server.host,
+          this.settings.server.port,
+          "ws-denied-max-connections url:",
+          request.getUrl(),
+          "query:",
+          request.getQuery(),
+          "origin:",
+          request.getHeader("origin"),
+          "total:",
+          this.webSocketsCount,
+        );
+      }
+
+      response.close();
+      return;
+    }
+
+    if (debugWebSocketsEnabled) {
+      debugWebSockets("connected via URL", request.getUrl());
+    }
+
+    if (this.validateOrigin) {
+      const origin = request.getHeader("origin");
+
+      const shoulDeny =
+        (this.settings.access.denyEmptyOrigin && origin.length === 0) ||
+        this.settings.access.denyOrigins?.includes(origin) === true ||
+        this.settings.access.allowOrigins?.includes(origin) === false;
+
+      if (shoulDeny) {
+        if (debugRequestsEnabled) {
+          debugRequests(
+            this.settings.server.host,
+            this.settings.server.port,
+            "ws-denied url:",
+            request.getUrl(),
+            "query:",
+            request.getQuery(),
+            "origin:",
+            origin,
+            "total:",
+            this.webSocketsCount,
+          );
         }
 
-        if (debugMessagesEnabled) {
-            debugMessages(
-                "in",
-                (userData.id === undefined) ? "unknown peer" : Buffer.from(userData.id!).toString("hex"),
-                json,
-            );
-        }
+        response.close();
+        return;
+      }
+    }
 
-        try {
-            this.tracker.processMessage(json, userData);
-        } catch (e) {
-            if (e instanceof TrackerError) {
-                debugWebSockets("failed to process message from the peer:", e);
-                ws.close();
-            } else {
-                throw e;
-            }
-        }
-    };
+    if (debugRequestsEnabled) {
+      debugRequests(
+        this.settings.server.host,
+        this.settings.server.port,
+        "ws-open url:",
+        request.getUrl(),
+        "query:",
+        request.getQuery(),
+        "origin:",
+        request.getHeader("origin"),
+        "total:",
+        this.webSocketsCount,
+      );
+    }
 
-    private readonly onClose = (ws: WebSocket<PeerContext>, code: number): void => {
-        this.webSocketsCount--;
+    response.upgrade<Pick<PeerContext, "sendMessage">>(
+      {
+        sendMessage,
+      },
+      request.getHeader("sec-websocket-key"),
+      request.getHeader("sec-websocket-protocol"),
+      request.getHeader("sec-websocket-extensions"),
+      context,
+    );
+  };
 
-        if (ws.getUserData().sendMessage !== undefined) {
-            this.tracker.disconnectPeer(ws as unknown as PeerContext);
-        }
+  private readonly onMessage = (
+    ws: WebSocket<PeerContext>,
+    message: ArrayBuffer,
+  ): void => {
+    debugWebSockets("message of size", message.byteLength);
 
-        debugWebSockets("closed with code", code);
-    };
+    const userData = ws.getUserData();
+    userData.ws = ws;
+
+    let json: object | undefined = undefined;
+    try {
+      json = JSON.parse(
+        decoder.end(new Uint8Array(message) as Buffer),
+      ) as object;
+    } catch (e) {
+      debugWebSockets("failed to parse JSON message", e);
+      ws.close();
+      return;
+    }
+
+    if (debugMessagesEnabled) {
+      debugMessages(
+        "in",
+        userData.id === undefined
+          ? "unknown peer"
+          : Buffer.from(userData.id!).toString("hex"),
+        json,
+      );
+    }
+
+    try {
+      this.tracker.processMessage(json, userData);
+    } catch (e) {
+      if (e instanceof TrackerError) {
+        debugWebSockets("failed to process message from the peer:", e);
+        ws.close();
+      } else {
+        throw e;
+      }
+    }
+  };
+
+  private readonly onClose = (
+    ws: WebSocket<PeerContext>,
+    code: number,
+  ): void => {
+    this.webSocketsCount--;
+
+    if (ws.getUserData().sendMessage !== undefined) {
+      this.tracker.disconnectPeer(ws as unknown as PeerContext);
+    }
+
+    debugWebSockets("closed with code", code);
+  };
 }
 
 function sendMessage(json: object, peerContext: PeerContext): void {
-    peerContext.ws.send(JSON.stringify(json), false, false);
-    if (debugMessagesEnabled) {
-        debugMessages(
-            "out",
-            (peerContext.id === undefined) ? "unknown peer" : Buffer.from(peerContext.id!).toString("hex"),
-            json,
-        );
-    }
+  peerContext.ws.send(JSON.stringify(json), false, false);
+  if (debugMessagesEnabled) {
+    debugMessages(
+      "out",
+      peerContext.id === undefined
+        ? "unknown peer"
+        : Buffer.from(peerContext.id!).toString("hex"),
+      json,
+    );
+  }
 }
--- test/performance/announce.ts
+++ test/performance/announce.ts
@@ -23,97 +23,97 @@ const offersCount = 10;
 const peers: any[] = [];
 
 const message = {
-    action: "announce",
-    event: "started",
-    info_hash: "hashhashhashhashhash",
-    peer_id: "",
-    offers: new Array<any>(),
-    numwant: offersCount,
+  action: "announce",
+  event: "started",
+  info_hash: "hashhashhashhashhash",
+  peer_id: "",
+  offers: new Array<any>(),
+  numwant: offersCount,
 };
 
 for (let i = 0; i < offersCount; i++) {
-    message.offers.push({
-        offer: {
-            sdp: "x",
-            value: 1,
-        },
-        offer_id: "t",
-    });
+  message.offers.push({
+    offer: {
+      sdp: "x",
+      value: 1,
+    },
+    offer_id: "t",
+  });
 }
 
 function addingPeersToSwarm(tracker: Tracker) {
-    peers.length = 0;
-    for (let i = 0; i < peersCount; i++) {
-        peers.push({
-            sendMessage: sendMessage,
-            _peerId: i.toPrecision(19),
-        });
-    }
+  peers.length = 0;
+  for (let i = 0; i < peersCount; i++) {
+    peers.push({
+      sendMessage: sendMessage,
+      _peerId: i.toPrecision(19),
+    });
+  }
 
-    console.time(`adding peers to a swarm ${tracker.constructor.name}`);
-    for (let i = 0; i < peersCount; i++) {
-        const peer = peers[i];
-        message.peer_id = peer._peerId;
-        tracker.processMessage(message, peer);
-    }
-    console.timeEnd(`adding peers to a swarm ${tracker.constructor.name}`);
+  console.time(`adding peers to a swarm ${tracker.constructor.name}`);
+  for (let i = 0; i < peersCount; i++) {
+    const peer = peers[i];
+    message.peer_id = peer._peerId;
+    tracker.processMessage(message, peer);
+  }
+  console.timeEnd(`adding peers to a swarm ${tracker.constructor.name}`);
 }
 
 function addingPeersToSwarmReference() {
-    peers.length = 0;
-    for (let i = 0; i < peersCount; i++) {
-        peers.push({
-            id: i.toPrecision(19),
-        });
-    }
+  peers.length = 0;
+  for (let i = 0; i < peersCount; i++) {
+    peers.push({
+      id: i.toPrecision(19),
+    });
+  }
 
-    const swarm = new Map<string, any>();
-    const peersOrdered: any[] = [];
-    let counter = 0;
+  const swarm = new Map<string, any>();
+  const peersOrdered: any[] = [];
+  let counter = 0;
 
-    console.time("adding peers to a swarm reference");
-    for (let p = 0; p < peersCount; p++) {
-        const peer = peers[p];
+  console.time("adding peers to a swarm reference");
+  for (let p = 0; p < peersCount; p++) {
+    const peer = peers[p];
 
-        swarm.set(peer.id, peer);
-        peersOrdered.push(peer);
+    swarm.set(peer.id, peer);
+    peersOrdered.push(peer);
 
-        message.peer_id = peer._peerId;
-        const offers = message.offers;
-        const countOffersToSend = Math.min(swarm.size - 1, offers.length, 20);
-        if (countOffersToSend === swarm.size - 1) {
-            const offersIterator = offers.values();
-            for (const toPeer of swarm.values()) {
-                if (toPeer !== peer) {
-                    counter += offersIterator.next().value.offer.value;
-                }
-            }
-        } else {
-            let peerIndex = Math.floor(Math.random() * swarm.size);
-
-            // send offers to random peers
-            for (let i = 0; i < countOffersToSend; i++) {
-                const toPeer = peersOrdered[peerIndex];
-
-                peerIndex++;
-                if (peerIndex === swarm.size) {
-                    peerIndex = 0;
-                }
-
-                if (toPeer === peer) {
-                    i--; // do one more iteration
-                } else {
-                    counter += offers[i].offer.value;
-                }
-            }
+    message.peer_id = peer._peerId;
+    const offers = message.offers;
+    const countOffersToSend = Math.min(swarm.size - 1, offers.length, 20);
+    if (countOffersToSend === swarm.size - 1) {
+      const offersIterator = offers.values();
+      for (const toPeer of swarm.values()) {
+        if (toPeer !== peer) {
+          counter += offersIterator.next().value.offer.value;
         }
+      }
+    } else {
+      let peerIndex = Math.floor(Math.random() * swarm.size);
+
+      // send offers to random peers
+      for (let i = 0; i < countOffersToSend; i++) {
+        const toPeer = peersOrdered[peerIndex];
+
+        peerIndex++;
+        if (peerIndex === swarm.size) {
+          peerIndex = 0;
+        }
+
+        if (toPeer === peer) {
+          i--; // do one more iteration
+        } else {
+          counter += offers[i].offer.value;
+        }
+      }
     }
-    console.timeEnd("adding peers to a swarm reference");
-    console.log(counter);
+  }
+  console.timeEnd("adding peers to a swarm reference");
+  console.log(counter);
 }
 
 for (let i = 0; i < 10; i++) {
-    addingPeersToSwarm(new FastTracker({}));
-    addingPeersToSwarmReference();
-    console.log("---------");
+  addingPeersToSwarm(new FastTracker({}));
+  addingPeersToSwarmReference();
+  console.log("---------");
 }
